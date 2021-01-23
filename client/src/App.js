5d3aae6fd18ad19864bb3275a57698d5487897e1
anthonyvseth <anthonyvseth@gmail.com>
2021-01-22 18:19:54 -0700

    created sign In function

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,24 @@ import 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 
+const{
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASURMENT_ID
+  }=process.env
+
 firebase.initializeApp({
-
-
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASURMENT_ID
 })
 
 const auth = firebase.auth()
@@ -18,13 +33,31 @@ const firestore = firebase.firestore()
 
 
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
 
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign In with Google</button>
+  )
 }
 
 export default App;
